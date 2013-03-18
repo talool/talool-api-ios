fe0b6f1f6f3666f353bcd3c371245adc10ca55f7
douglasmccuen <douglasmccuen@gmail.com>
2013-03-17 21:40:34 -0600

    refactored for better core data support

--- talool-api-ios/talool-api-ios/CustomerController.m
+++ talool-api-ios/talool-api-ios/CustomerController.m
@@ -15,6 +15,7 @@
 #import "Core.h"
 #import "CustomerService.h"
 #import "TaloolFrameworkHelper.h"
+#import "TaloolPersistentStoreCoordinator.h"
 
 @implementation CustomerController
 
@@ -34,10 +35,10 @@
     TBinaryProtocol *protocol;
     @try {
         // Talk to a server via socket, using a binary protocol
-        /* 
+        /*
          NOTE: If you try to connect to a server/port that is down,
-               the phone will crash with a EXC_BAD_ACCESS when this
-               controller is gabage collected.
+         the phone will crash with a EXC_BAD_ACCESS when this
+         controller is gabage collected.
          */
         NSURL *url = [NSURL URLWithString:API_URL];
         transport = [[THTTPClient alloc] initWithURL:url];
@@ -75,49 +76,75 @@
     
     // validate data before sending to the server
     if (![customer isValid:error]){
-        return NO;
+        return nil;
     }
-
+    
     // Convert the core data obj to a thrift object
     Customer_t *newCustomer = [customer hydrateThriftObject];
+    CTokenAccess_t *token;
     
     NSMutableDictionary* details = [NSMutableDictionary dictionary];
     
     @try {
         // Do the Thrift Registration
         [self connect];
-        CTokenAccess_t *token = [service createAccount:newCustomer password:customer.password];
-        ttToken *ttt = [ttToken initWithThrift:token context:context];
-        customer = (ttCustomer *)ttt.customer;
-        customer.token = ttt;
+        token = [service createAccount:newCustomer password:customer.password];
     }
     @catch (ServiceException_t * se) {
         [details setValue:@"Failed to register user, service failed." forKey:NSLocalizedDescriptionKey];
         *error = [NSError errorWithDomain:@"registration" code:200 userInfo:details];
         NSLog(@"failed to complete registration cycle: %@",se.description);
-        return NO;
+        return nil;
     }
     @catch (TApplicationException * tae) {
         [details setValue:@"Failed to register user; app failed." forKey:NSLocalizedDescriptionKey];
         *error = [NSError errorWithDomain:@"registration" code:200 userInfo:details];
         NSLog(@"failed to complete registration cycle: %@",tae.description);
-        return NO;
+        return nil;
     }
     @catch (TTransportException * tpe) {
         [details setValue:@"Failed to register user, cuz the server barfed." forKey:NSLocalizedDescriptionKey];
         *error = [NSError errorWithDomain:@"registration" code:200 userInfo:details];
         NSLog(@"failed to complete registration cycle: %@",tpe.description);
-        return NO;
+        return nil;
     }
     @catch (NSException * e) {
         [details setValue:@"Failed to register user... who knows why." forKey:NSLocalizedDescriptionKey];
         *error = [NSError errorWithDomain:@"registration" code:200 userInfo:details];
         NSLog(@"failed to complete registration cycle: %@",e.description);
-        return NO;
+        return nil;
     }
     @finally {
-        NSLog(@"completed registration cycle");
+        NSLog(@"API: completed registration cycle for customerId:%lld and token:%@", token.customer.customerId, token.token);
         [self disconnect];
+        
+        // delete all TaloolCustomer objects
+        NSFetchRequest *request = [[NSFetchRequest alloc] init];
+        NSEntityDescription *entity = [NSEntityDescription entityForName:CUSTOMER_ENTITY_NAME inManagedObjectContext:context];
+        [request setEntity:entity];
+        NSError *error = nil;
+        NSMutableArray *mutableFetchResults = [[context executeFetchRequest:request error:&error] mutableCopy];
+        if ([mutableFetchResults count] > 0) {
+            for (NSManagedObject *extra_user in mutableFetchResults) {
+                [context deleteObject:extra_user];
+            }
+        }
+    }
+    
+    @try {
+        // transform the Thrift response into a ttCustomer
+        ttToken *ttt = [ttToken initWithThrift:token context:context];
+        customer = (ttCustomer *)ttt.customer;
+        customer.token = ttt;
+        NSLog(@"API: transformed token for customerId:%d and token:%@", [ttt.customer.customerId intValue], customer.token.token);
+        NSError *err = nil;
+        [context save:&err];
+    }
+    @catch (NSException * e) {
+        [details setValue:@"Failed to create the customer object." forKey:NSLocalizedDescriptionKey];
+        *error = [NSError errorWithDomain:@"post-reg" code:200 userInfo:details];
+        NSLog(@"failed to hydrate the user: %@",e.description);
+        return nil;
     }
     
     return customer;
@@ -127,42 +154,54 @@
 - (ttCustomer *)authenticate:(NSString *)email password:(NSString *)password context:(NSManagedObjectContext *)context error:(NSError**)error;
 {
     ttCustomer *customer;
+    CTokenAccess_t *token;
     NSMutableDictionary* details = [NSMutableDictionary dictionary];
+    
     @try {
         // Do the Thrift Authentication
         [self connect];
-        CTokenAccess_t *token = [service authenticate:email password:password];
-        ttToken *ttt = [ttToken initWithThrift:token context:context];
-        customer = (ttCustomer *)ttt.customer;
-        customer.token = ttt;
+        token = [service authenticate:email password:password];
     }
     @catch (ServiceException_t * se) {
         [details setValue:@"Failed to authenticate user, service failed." forKey:NSLocalizedDescriptionKey];
         *error = [NSError errorWithDomain:@"login" code:200 userInfo:details];
         NSLog(@"failed to complete login cycle: %@",se.description);
-        return NO;
+        return nil;
     }
     @catch (TApplicationException * tae) {
         [details setValue:@"Failed to authenticate user; app failed." forKey:NSLocalizedDescriptionKey];
         *error = [NSError errorWithDomain:@"login" code:200 userInfo:details];
         NSLog(@"failed to complete login cycle: %@",tae.description);
-        return NO;
+        return nil;
     }
     @catch (TTransportException * tpe) {
         [details setValue:@"Failed to authenticate user, cuz the server barfed." forKey:NSLocalizedDescriptionKey];
         *error = [NSError errorWithDomain:@"login" code:200 userInfo:details];
         NSLog(@"failed to complete login cycle: %@",tpe.description);
-        return NO;
+        return nil;
     }
     @catch (NSException * e) {
         [details setValue:@"Failed to authenticate user... who knows why." forKey:NSLocalizedDescriptionKey];
         *error = [NSError errorWithDomain:@"login" code:200 userInfo:details];
         NSLog(@"failed to complete login cycle: %@",e.description);
-        return NO;
+        return nil;
     }
     @finally {
         NSLog(@"completed login cycle");
         [self disconnect];
+    }
+    
+    @try {
+        // transform the Thrift response into a ttCustomer
+        ttToken *ttt = [ttToken initWithThrift:token context:context];
+        customer = (ttCustomer *)ttt.customer;
+        customer.token = ttt;
+    }
+    @catch (NSException * e) {
+        [details setValue:@"Failed to create the customer object." forKey:NSLocalizedDescriptionKey];
+        *error = [NSError errorWithDomain:@"post-login" code:200 userInfo:details];
+        NSLog(@"failed to hydrate the user: %@",e.description);
+        return nil;
     }
     
     return customer;
