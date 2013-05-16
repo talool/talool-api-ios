eeb68d132ed37cfd691bd9d169a81937604dace4
douglasmccuen <douglasmccuen@gmail.com>
2013-05-16 14:36:46 -0600

    adding location

--- talool-api-ios/talool-api-ios/talool_api_ios.h
+++ talool-api-ios/talool-api-ios/talool_api_ios.h
@@ -11,6 +11,7 @@
 #import <talool-api-ios/ttCustomer.h>
 #import <talool-api-ios/ttToken.h>
 #import <talool-api-ios/ttAddress.h>
+#import <talool-api-ios/ttLocation.h>
 #import <talool-api-ios/ttMerchant.h>
 #import <talool-api-ios/ttMerchantLocation.h>
 #import <talool-api-ios/ttDeal.h>
